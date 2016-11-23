d334f1e1164de50e6c443118028e87afd8fd7883
yvo <yvo@eveg.vegian>
2016-11-23 14:11:05 +0100

    modification de page d'accueuil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+def  bonjour
+render html: "bonjour, bienvenu sur le super serveur"
+
+end
